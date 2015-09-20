0986b41bebec8cdee7591424a2b210e745eaa3ea
johan <josanuz@hotmail.es>
2015-09-19 20:11:18 -0600

    SkyBox added

--- src/js/main.js
+++ src/js/main.js
@@ -68,7 +68,16 @@ var initScene = function() {
         TOAD_MODEL = m;
     });
 
-
+/**SkyBox**/
+var skybox = BABYLON.Mesh.CreateBox("skyBox", 1000.0, scene);
+    var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
+    skyboxMaterial.backFaceCulling = false;
+    skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("textures/skybox", scene);
+    skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+    skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+    skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+    skybox.material = skyboxMaterial;
+/********/
     initGame();
 
 
